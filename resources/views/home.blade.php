379ceffb2be01abd494abbbefd667b77a6ea739d
ArisGoi <aris.goi97@gmail.com>
2021-11-05 18:36:21 +0100

    add header

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.base')
+
+@section('pageTitle')
+    Homepage
+@endsection
+
+@section('pageContent')
+
+<h1>titolo</h1>
+<p>contenuto pagina</p>
+
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,17 @@
+<header>
+    <nav class="container flex-ori">
+        <img class="logo" src="{{asset('images/dc-logo.png')}}" alt="Logo DC">
+        <ul>
+            <li><a href="#">Characters</a></li>
+            <li class="current"><a href="{{route('home')}}">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Collectibles</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">Fans</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">Shop</a></li>
+        </ul>
+    </nav>
+</header>
